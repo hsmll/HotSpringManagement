58837e252e5318ae89ba95f169fdfdfc7b1dc81e
W-cece <1434160536@qq.com>
2019-11-06 18:10:13 +0800

    测试

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
+<<<<<<< Updated upstream:src/views/test.vue
     <h2>test</h2>
+=======
+    1111
+>>>>>>> Stashed changes:src/views/abcd.vue
   </div>
 </template>
 
 <script>
+ Updated upstream:src/views/test.vue
     export default {
         name: "test"
     }
+
+export default {
+  name: 'Abcd'
+}
+Stashed changes:src/views/abcd.vue
 </script>
 
 <style scoped>
